bb7ec8502c1d98f48efa37171bcf02d2fc867293
Kim JaeSeok <iai6203@gmail.com>
2024-04-16 15:13:08 +0900

    feat: 강좌 Grid & 카드 UI

--- src/app/(dashboard)/(routes)/search/page.tsx
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -1,15 +1,39 @@
 import React from "react"
+import { redirect } from "next/navigation"
+import { auth } from "@clerk/nextjs"
 
 import { SearchInput } from "@/components/search-input"
+import { CoursesList } from "@/components/courses-list"
+import { getCourses } from "@/actions/get-courses-props"
 import { db } from "@/lib/db"
 
 import { Categories } from "./_components/categories"
 
-export default async function SearchPage() {
+interface SearchPageProps {
+  searchParams: {
+    title?: string
+    categoryId?: string
+  }
+}
+
+export default async function SearchPage({
+  searchParams,
+}: SearchPageProps) {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect("/")
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
+  })
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
   })
 
   return (
@@ -17,10 +41,11 @@ export default async function SearchPage() {
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
         <Categories
           items={categories}
         />
+        <CoursesList items={courses} />
       </div>
     </>
   )
