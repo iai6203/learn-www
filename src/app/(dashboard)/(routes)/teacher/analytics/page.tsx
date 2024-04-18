d55da7d0a8893b396d600aa8f04249c381170dd7
Kim JaeSeok <iai6203@gmail.com>
2024-04-18 17:12:09 +0900

    feat: 강사 통계 대시보드

--- src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,7 +1,39 @@
 import React from "react"
+import { redirect } from "next/navigation"
+import { auth } from "@clerk/nextjs"
 
-export default function AnalyticsPage() {
+import { getAnalytics } from "@/actions/get-analytics"
+
+import { DataCard } from "./_components/data-card"
+import { Chart } from "./_components/chart"
+
+export default async function AnalyticsPage() {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect("/")
+  }
+
+  const {
+    data,
+    totalRevenue,
+    totalSales,
+  } = await getAnalytics(userId)
+
   return (
-    <div>통계 페이지</div>
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard
+          label="총 수익"
+          value={totalRevenue}
+          shouldFormat
+        />
+        <DataCard
+          label="총 판매 수"
+          value={totalSales}
+        />
+      </div>
+      <Chart data={data} />
+    </div>
   )
 }
